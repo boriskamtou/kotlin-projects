dfc5b2f225edd02cc214440cb154b3f5c254045d
Boris_K <b.kamtou@pkf-researchcenter.com>
2024-01-17 21:19:46 +0100

    complete project

--- CoursApp/app/src/main/java/com/wcs/cours_app/MainActivity.kt
+++ CoursApp/app/src/main/java/com/wcs/cours_app/MainActivity.kt
@@ -3,13 +3,33 @@ package com.wcs.cours_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wcs.cours_app.model.Topic
 import com.wcs.cours_app.ui.theme.CoursAppTheme
 
@@ -32,24 +52,74 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TopicApp() {
-
+    TopicList(
+        topics = DataSource.topics,
+        modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))
+    )
 }
 
 @Composable
-fun TopicList(topics: ) {
-
+fun TopicList(topics: List<Topic>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        modifier = modifier
+    ) {
+        items(topics) { topic ->
+            TopicItem(topic = topic, modifier = Modifier.padding(bottom = 8.dp))
+        }
+    }
 }
 
 
 @Composable
 fun TopicItem(topic: Topic, modifier: Modifier = Modifier) {
+    Card(modifier = modifier.fillMaxWidth()) {
+        Row {
+            Box {
+                Image(
+                    painter = painterResource(id = topic.courseImage),
+                    contentDescription = stringResource(
+                        id = R.string.architecture
+                    ),
+                    modifier = modifier
+                        .size(width = 68.dp, height = 68.dp)
+                        .aspectRatio(1f),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Spacer(modifier = modifier.padding(end = 16.dp))
+            Column(
+                modifier = Modifier.padding(end = 16.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = stringResource(id = topic.courseName),
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+                Spacer(modifier = Modifier.padding(bottom = 8.dp))
+                Row {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = null,
 
+                        )
+                    Spacer(modifier = Modifier.padding(end = 8.dp))
+                    Text(
+                        text = topic.nbCourses.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                    )
+                }
+            }
+        }
+    }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TopicAppPreview() {
     CoursAppTheme {
         TopicApp()
     }
